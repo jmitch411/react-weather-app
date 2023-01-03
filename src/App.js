8f1b7ff77c89100f05269579f817ff30248cdf22
jmitch411 <mitchelljasmin15@yahoo.com>
2023-01-03 09:18:43 -0500

    Added GitHub open-source link at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This was coded by Jasmin Mitchell and is{" "}
+        <a href="https://github.com/jmitch411/react-weather-app" target="_blank" rel="noreffer">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
